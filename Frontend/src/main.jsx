aaefcd3b76a8d2ceab37c645da5d8c073918931f
CV <brunchwithyou@gmail.com>
2022-01-14 11:07:27 -0800

    trying out vercel analytics

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 if (process.env.NODE_ENV === "production") {
   disableReactDevTools();
