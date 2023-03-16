32f929a770a9fd84feafce00cce32884eb143c67
CV <brunchwithyou@gmail.com>
2023-03-15 21:39:43 -0700

    Finished testing auth api endpoints, added middleware to protected routes.

--- BackEnd/routes/equipmentRoutes.js
+++ BackEnd/routes/equipmentRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const equipmentController = require("../controllers/equipmentController");
+const verifyJWT = require("../middlewares/verifyJWT");
+
+router.use(verifyJWT);
 
 router.get("/", equipmentController.getAllEquipment);
 router.post("/", equipmentController.createEquipment);
