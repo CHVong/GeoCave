1ab58fd3d8f9d8cda7a0692f4231d299c61cf886
CV <brunchwithyou@gmail.com>
2023-04-05 22:13:37 -0700

    Added styling for checklist and placeholders for checklist items

---
+++ Frontend/src/components/FormCheck.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const FormCheck = ({ id, title }) => {
+  return (
+    <div className="text-left animate-fadeIn flex flex-row items-center gap-4 m-2">
+      <input
+        type="checkbox"
+        id={id}
+        className="focus:ring-0 focus:ring-offset-0 rounded-full w-6 h-6 checked:text-green-700 peer transition bg-gray-800 cursor-pointer"
+      />
+      <label
+        for={id}
+        className="p-2 peer-checked:text-gray-500 peer-checked:line-through peer-checked:decoration-green-700 transition cursor-pointer w-full"
+      >
+        {title}
+      </label>
+    </div>
+  );
+};
+
+export default FormCheck;
--- Frontend/src/pages/content/Drilling.jsx
+++ Frontend/src/pages/content/Drilling.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import FormCheck from "../../components/FormCheck";
 
 const Drilling = () => {
-  return <div>Drilling</div>;
+  return (
+    <div>
+      <h1 className="m-4 underline text-xl font-bold">Required</h1>
+      <FormCheck id={"1"} title={`Rings (2.5"x1")`} />
+      <FormCheck id={"2"} title={`Ring Sleeves (2.5"x6")`} />
+      <FormCheck id={"3"} title={"Plastic liners to bag ring samples"} />
+      <FormCheck id={"4"} title={"Bulk bags"} />
+      <FormCheck id={"5"} title={"SPT bags"} />
+      <FormCheck id={"6"} title={"Paperwork (PTP, logs, proposal, permits, etc.)"} />
+      <FormCheck
+        id={"7"}
+        title={`Writing/labeling supplies (Pens, sharpies, clipboard, masking tape)
+`}
+      />
+    </div>
+  );
 };
 
 export default Drilling;
