675c7fa3a75bb1129be14778e26dc47248e94c1f
CV <brunchwithyou@gmail.com>
2023-05-01 14:20:35 -0700

    Finished adding states, inputs and calculations for percolation/infiltration rate analysis

--- Frontend/src/pages/content/PercolationAnalysis.jsx
+++ Frontend/src/pages/content/PercolationAnalysis.jsx
@@ -2,16 +2,57 @@ import { useState, useEffect } from "react";
 
 const PercolationAnalysis = () => {
   const [wellDepth, setWellDepth] = useState("");
-  const [DTW, setDTW] = useState("");
-  const [initialDTW, setInitialDTW] = useState("");
-  const [finalDTW, setFinalDTW] = useState("");
-  const [head, setHead] = useState("");
+  const [initialDTW, setInitialDTW] = useState("Require Readings and Stick-Up");
+  const [finalDTW, setFinalDTW] = useState("Require Readings and Stick-Up");
+  const [initialReading, setInitialReading] = useState("");
+  const [finalReading, setFinalReading] = useState("");
+  const [wellStickUp, setWellStickUp] = useState("");
+  const [head, setHead] = useState("Require Depth, Initial, and Final DTW");
   const [deltaTime, setDeltaTime] = useState("");
   const [casingDiameter, setCasingDiameter] = useState("");
-  const [deltaWaterLevel, setDeltaWaterLevel] = useState("Please Enter Initial and Final DTW");
-  const [percolationRate, setPercolationRate] = useState("");
-  const [infiltrationRate, setInfiltrationRate] = useState("");
-  const [wellStickUp, setWellStickUp] = useState("");
+  const [deltaWaterLevel, setDeltaWaterLevel] = useState("Require Readings");
+  const [percolationRate, setPercolationRate] = useState("Require Time and Δ Water Level");
+  const [infiltrationRate, setInfiltrationRate] = useState("Require All Fields");
+
+  useEffect(() => {
+    if (initialReading !== "" && finalReading !== "" && wellStickUp !== "") {
+      let newInitial = +initialReading - +wellStickUp;
+      let newFinal = +finalReading - +wellStickUp;
+      setInitialDTW(+newInitial.toFixed(4));
+      setFinalDTW(+newFinal.toFixed(4));
+    } else {
+      setInitialDTW("Require Readings and Stick-Up");
+      setFinalDTW("Require Readings and Stick-Up");
+    }
+  }, [initialReading, finalReading, wellStickUp]);
+  useEffect(() => {
+    if (initialReading !== "" && finalReading !== "") {
+      setDeltaWaterLevel(+(finalReading - initialReading).toFixed(4));
+    } else setDeltaWaterLevel("Require Initial and Final Readings");
+  }, [initialReading, finalReading]);
+  useEffect(() => {
+    if (initialDTW !== "" && finalDTW !== "" && wellDepth !== "" && wellStickUp !== "") {
+      let average = (+finalDTW + +initialDTW) / 2;
+      let depthInches = wellDepth * 12;
+      setHead(+(depthInches - average).toFixed(4));
+    } else setHead("Require Depth, Initial, and Final DTW");
+  }, [initialDTW, finalDTW, wellDepth]);
+  useEffect(() => {
+    if (deltaWaterLevel !== "" && deltaTime !== "") {
+      setPercolationRate(+((deltaWaterLevel / deltaTime) * 60).toFixed(4));
+    } else setPercolationRate("Require Time and Δ Water Level");
+  }, [deltaTime, deltaWaterLevel]);
+  useEffect(() => {
+    if (deltaTime !== "" && casingDiameter !== "" && head !== "") {
+      setInfiltrationRate(
+        +(
+          (deltaWaterLevel * 60 * casingDiameter) /
+          2 /
+          (deltaTime * (casingDiameter / 2 + 2 * head))
+        ).toFixed(4)
+      );
+    } else setInfiltrationRate("Require All Fields");
+  }, [casingDiameter, wellStickUp, wellDepth, deltaTime, initialReading, finalReading]);
 
   return (
     <div className="flex flex-col gap-4 animate-fadeIn">
@@ -59,7 +100,7 @@ const PercolationAnalysis = () => {
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="wellDepth" className="text-left italic">
-            Well Depth:
+            Well Depth(TD):
           </label>
           <div className="flex gap-2 items-center justify-start">
             <input
@@ -98,40 +139,40 @@ const PercolationAnalysis = () => {
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="initialDTW" className="text-left italic">
-            Initial Depth-to-Water:
+          <label htmlFor="initialReading" className="text-left italic">
+            Initial TOC Reading:
           </label>
           <div className="flex gap-2 items-center justify-start">
             <input
               type="number"
-              name="initialDTW"
-              id="initialDTW"
+              name="initialReading"
+              id="initialReading"
               autoComplete="off"
               placeholder="Inches"
               step="1"
-              value={initialDTW}
+              value={initialReading}
               required
-              onChange={(e) => setInitialDTW(e.target.value)}
+              onChange={(e) => setInitialReading(e.target.value)}
               className={`rounded px-2 py-2 bg-black outline-none ring-1`}
             />
             <h2 className="text-sm">Inches</h2>
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="finalDTW" className="text-left italic">
-            Final Depth-to-Water:
+          <label htmlFor="finalReading" className="text-left italic">
+            Final TOC Reading:
           </label>
           <div className="flex gap-2 items-center justify-start">
             <input
               type="number"
-              name="finalDTW"
-              id="finalDTW"
+              name="finalReading"
+              id="finalReading"
               autoComplete="off"
               placeholder="Inches"
               step="1"
-              value={finalDTW}
+              value={finalReading}
               required
-              onChange={(e) => setFinalDTW(e.target.value)}
+              onChange={(e) => setFinalReading(e.target.value)}
               className={`rounded px-2 py-2 bg-black outline-none ring-1`}
             />
             <h2 className="text-sm">Inches</h2>
@@ -141,13 +182,63 @@ const PercolationAnalysis = () => {
       <div className="border-2 border-primary m-auto rounded-lg p-4 mt-8">
         <h2 className="text-lg font-bold">Calculated Values</h2>
         <h2 className="font-bold">
-          &#916;Water Level:{" "}
+          Initial DTW (Ft) :{" "}
+          <span
+            className={`font-normal ${
+              typeof initialDTW === "number" ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {initialDTW}
+          </span>
+        </h2>
+        <h2 className="font-bold">
+          Final DTW (Ft) :{" "}
+          <span
+            className={`font-normal ${
+              typeof finalDTW === "number" ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {finalDTW}
+          </span>
+        </h2>
+        <h2 className="font-bold">
+          &#916;Water Level (Ft) :{" "}
           <span
             className={`font-normal ${
               typeof deltaWaterLevel === "number" ? "text-green-500" : "text-red-500"
             }`}
           >
             {deltaWaterLevel}
+          </span>
+        </h2>
+        <h2 className="font-bold">
+          Head (In) :{" "}
+          <span
+            className={`font-normal ${
+              typeof head === "number" ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {head}
+          </span>
+        </h2>
+        <h2 className="font-bold">
+          Percolation Rate (In/Hr) :{" "}
+          <span
+            className={`font-normal ${
+              typeof percolationRate === "number" ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {percolationRate}
+          </span>
+        </h2>
+        <h2 className="font-bold">
+          Infiltration Rate (In/Hr) :{" "}
+          <span
+            className={`font-normal ${
+              typeof infiltrationRate === "number" ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {infiltrationRate}
           </span>
         </h2>
       </div>
