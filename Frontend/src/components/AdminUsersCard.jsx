4e067b0d229a608d96cf90343dc84a899d2dee5b
CV <brunchwithyou@gmail.com>
2023-05-18 14:57:48 -0700

    Fixed styling for available roles and custom roles after edit has been enabled. Added input for custom roles

--- Frontend/src/components/AdminUsersCard.jsx
+++ Frontend/src/components/AdminUsersCard.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 import { faPenToSquare, faFloppyDisk, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const availableRoles = ["Employee", "Admin", "Manager"];
+const availableRoles = ["Employee", "Manager", "Admin"];
 
 const AdminUsersCard = ({ id, username, roles, active, createdAt, updatedAt }) => {
   const [editRoles, setEditRoles] = useState(false);
@@ -44,7 +44,7 @@ const AdminUsersCard = ({ id, username, roles, active, createdAt, updatedAt }) =
           </div>
         </div>
 
-        <div className="flex gap-2 flex-wrap p-2">
+        <div className="flex gap-2 flex-wrap items-center p-2">
           {!editRoles &&
             roles.map((e, i) => {
               return (
@@ -64,26 +64,41 @@ const AdminUsersCard = ({ id, username, roles, active, createdAt, updatedAt }) =
                 </div>
               );
             })}
-          {editRoles &&
-            [...new Set([...availableRoles, ...roles])].map((e, i) => {
-              return (
-                <div
-                  className={`text-xs font-medium border-2 rounded-2xl px-2 cursor-pointer transition-all ${
-                    e === "Employee" && roles.includes("Employee")
-                      ? "bg-blue-900 border-blue-500 hover:border-blue-400"
-                      : e === "Admin" && roles.includes("Admin")
-                      ? "bg-red-900 border-red-500 hover:border-red-400"
-                      : e === "Manager" && roles.includes("Manager")
-                      ? "bg-orange-900 border-orange-500 hover:border-orange-400"
-                      : !roles.includes(e)
-                      ? "bg-gray-900 border-gray-500 hover:border-gray-400"
-                      : "bg-teal-900 border-teal-500 hover:border-teal-400"
-                  }`}
-                >
-                  {e}
-                </div>
-              );
-            })}
+          {editRoles && (
+            <>
+              {[...new Set([...availableRoles, ...roles])].map((e, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={`text-xs font-medium border-2 rounded-2xl px-2 cursor-pointer transition-all ${
+                      e === "Employee" && roles.includes("Employee")
+                        ? "bg-blue-900 border-blue-500 hover:border-blue-400"
+                        : e === "Admin" && roles.includes("Admin")
+                        ? "bg-red-900 border-red-500 hover:border-red-400"
+                        : e === "Manager" && roles.includes("Manager")
+                        ? "bg-orange-900 border-orange-500 hover:border-orange-400"
+                        : !roles.includes(e)
+                        ? "bg-gray-900 border-gray-500 hover:border-gray-400"
+                        : "bg-teal-900 border-teal-500 hover:border-teal-400"
+                    }`}
+                  >
+                    {e}
+                  </div>
+                );
+              })}
+              <div className="w-full">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  autoComplete="off"
+                  placeholder="Custom Role"
+                  required
+                  className={`rounded-3xl bg-black outline-none ring-1 text-xs font-medium w-28 h-5 hover:cursor-pointer`}
+                />
+              </div>
+            </>
+          )}
         </div>
       </div>
       <div className="text-start mr-auto group/active">
