f8f265270c5384395a683cdd22a70e6a3ab4b351
CV <brunchwithyou@gmail.com>
2023-05-18 13:53:21 -0700

    Added edit button and save button to roles in manage users page

--- Frontend/src/components/AdminUsersCard.jsx
+++ Frontend/src/components/AdminUsersCard.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import { useState } from "react";
 import moment from "moment";
+import { faPenToSquare, faFloppyDisk, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AdminUsersCard = ({ id, username, roles, active, createdAt, updatedAt }) => {
+  const [editRoles, setEditRoles] = useState(false);
   return (
     <div className="border-b border-slate-700 px-10 grid sm:grid-cols-4 ">
       <div className="text-start group/username">
-        <div className="text-sm text-slate-500 group-hover/username:text-slate-300">Username</div>
+        <div className="text-sm text-slate-500 group-hover/username:text-slate-300 transition">
+          Username
+        </div>
         <div className="text-lg font-bold">{username}</div>
       </div>
       <div className="text-start mr-auto group/roles">
-        <div className="text-sm text-slate-500 group-hover/roles:text-slate-300">Roles</div>
+        <div className="flex gap-2 items-center text-sm text-slate-500">
+          <div className="group-hover/roles:text-slate-300 transition">Roles</div>
+          <div>
+            {roles.includes("Admin") ? (
+              ""
+            ) : (
+              <>
+                {!editRoles && (
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    className="hover:scale-90 hover:cursor-pointer transition hover:text-slate-300"
+                    onClick={() => setEditRoles(true)}
+                  />
+                )}
+                {editRoles ? (
+                  <FontAwesomeIcon
+                    icon={faFloppyDisk}
+                    className="hover:scale-90 hover:cursor-pointer transition hover:text-green-600"
+                    onClick={() => setEditRoles(false)}
+                  />
+                ) : (
+                  ""
+                )}
+              </>
+            )}
+          </div>
+        </div>
+
         <div className="flex gap-2 flex-wrap p-2">
           {roles.map((e, i) => {
             return (
               <div
-                className={`text-xs font-bold border-2 rounded-2xl px-2 cursor-pointer transition-all ${
+                className={`text-xs font-medium border-2 rounded-2xl px-2 cursor-pointer transition-all ${
                   e === "Employee"
                     ? "bg-blue-900 border-blue-500 hover:border-blue-400"
                     : e === "Admin"
@@ -29,10 +61,12 @@ const AdminUsersCard = ({ id, username, roles, active, createdAt, updatedAt }) =
         </div>
       </div>
       <div className="text-start mr-auto group/active">
-        <div className="text-sm text-slate-500 group-hover/active:text-slate-300">Status</div>
+        <div className="text-sm text-slate-500 group-hover/active:text-slate-300 transition">
+          Status
+        </div>
         <div className="flex gap-2 flex-wrap p-2">
           <div
-            className={`text-xs font-bold border-2 rounded-2xl px-2 cursor-pointer transition-all ${
+            className={`text-xs font-medium border-2 rounded-2xl px-2 cursor-pointer transition-all ${
               active
                 ? "bg-green-900 border-green-500 hover:border-green-400"
                 : active
@@ -45,7 +79,9 @@ const AdminUsersCard = ({ id, username, roles, active, createdAt, updatedAt }) =
         </div>
       </div>
       <div className="text-start group/created">
-        <div className="text-sm text-slate-500 group-hover/created:text-slate-300">Created</div>
+        <div className="text-sm text-slate-500 group-hover/created:text-slate-300 transition">
+          Created
+        </div>
         <div className="font-bold">{moment(createdAt).format("LLL")}</div>
       </div>
     </div>
