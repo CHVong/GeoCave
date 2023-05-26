67dbc2c4cfb525879255e83d25ad860b65673938
CV <brunchwithyou@gmail.com>
2023-05-26 00:32:41 -0700

    Added changes neccessary for api calls, added changes to prepare for github page deployment

--- BackEnd/config/allowedOrigins.js
+++ BackEnd/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://www.geocave.com",
   "http://localhost:5173",
+  "http://localhost:4173",
+  "https://brunchwithyou.github.io/GeoCave/",
 ];
 
 module.exports = allowedOrigins;
