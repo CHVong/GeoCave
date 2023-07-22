ae157ad26c7415cff2cf2e2731bd51aa4f0f2cf5
CV <brunchwithyou@gmail.com>
2023-07-22 16:18:47 -0700

    Added loading state for login button. Fixed for on success and on error.

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -9,6 +9,7 @@ const LOGIN_URL = "/auth";
 
 const Login = () => {
   const { setAuth, persist, setPersist, username, setUsername } = useAuth();
+  const [buttonLoad, setButtonLoad] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
@@ -30,6 +31,7 @@ const Login = () => {
   }, [user, pwd]);
 
   const handleSubmit = async (e) => {
+    setButtonLoad(true);
     e.preventDefault();
 
     try {
@@ -50,6 +52,7 @@ const Login = () => {
       setUsername(user);
       setUser("");
       setPwd("");
+      setButtonLoad(false);
       if (from === "/") {
         navigate("/dash", { replace: true });
       } else {
@@ -72,6 +75,7 @@ const Login = () => {
         setErrMsg("Login Failed");
       }
       errRef.current.focus();
+      setButtonLoad(false);
     }
   };
 
@@ -107,7 +111,7 @@ const Login = () => {
           {errMsg}
         </p>
       </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 relative">
         <label htmlFor="username" className="text-left italic">
           Username:
         </label>
@@ -154,6 +158,30 @@ const Login = () => {
           </button>
           <LinkButton path={""} name={"Cancel"} />
         </div>
+        {buttonLoad ? (
+          <svg
+            class="animate-spin h-5 w-5 text-white absolute  self-center top-[103%]"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        ) : (
+          ""
+        )}
       </form>
       <p>
         Need an Account?
